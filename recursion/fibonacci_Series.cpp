b7b5dc1a8c9aa039ffe97c0f3237315cbef8065b
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2024-12-12 18:58:17 +0530

    pushing mth term of fibonacci series

---
+++ recursion/fibonacci_Series.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n < 0)
+    {
+        return 0;
+    }
+
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n = 6;
+    cout << fibonacci(n);
+    return 0;
+};
