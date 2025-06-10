57bf2214ed2657a4e492d3a9b1c480e09bde365b
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-06-10 07:10:36 +0530

    formatting

--- linked_list/linked_list_setup.cpp
+++ linked_list/linked_list_setup.cpp
@@ -21,14 +21,14 @@
 #include <iostream>
 using namespace std;
 
+// Node class: Represents a node in the linked list
 class Node
 {
 public:
-    int data;
-    Node *next;
+    int data;      // Data of the node
+    Node *next;    // Pointer to the next node
 
-    // Constructor
-
+    // Constructor: Initializes data and sets next to NULL
     Node(int data)
     {
         this->data = data;
@@ -36,53 +36,59 @@ public:
     }
 };
 
+// LinkedList class: Manages the entire linked list
 class LinkedList
 {
-    // Attributes
-    Node *head;
-    Node *tail;
+    Node *head;    // Pointer to the first node (head)
+    Node *tail;    // Pointer to the last node (tail)
 
 public:
+    // Constructor: Initializes head and tail to NULL
     LinkedList()
     {
         this->head = NULL;
         this->tail = NULL;
     }
 
-    // Puhsing Forwrad in a linked list
+    // Insert a node at the front of the linked list
     void pushForward(int data)
     {
         Node *newNode = new Node(data);
         if (head == NULL)
         {
+            // If the list is empty, both head and tail point to newNode
             head = newNode;
             tail = newNode;
             return;
         }
         else
         {
+            // Insert newNode before the current head and update head
             newNode->next = head;
             head = newNode;
         }
     }
 
-    // Puhsing BackWard in a Linked List
+    // Insert a node at the end of the linked list
     void pushBackward(int data)
     {
         Node *newNode = new Node(data);
         if (head == NULL)
         {
+            // If the list is empty, both head and tail point to newNode
             head = newNode;
             tail = newNode;
             return;
         }
         else
         {
+            // Add newNode after the current tail and update tail
             tail->next = newNode;
             tail = newNode;
         }
     }
 
+    // Print the entire linked list
     void printLinkedList()
     {
         Node *tempNode = head;
@@ -101,18 +107,22 @@ public:
 int main()
 {
     LinkedList linkedList = LinkedList();
+
+    // Insert nodes at the front
     linkedList.pushForward(5);
     linkedList.pushForward(4);
     linkedList.pushForward(3);
     linkedList.pushForward(2);
     linkedList.pushForward(1);
 
+    // Insert nodes at the end
     linkedList.pushBackward(6);
     linkedList.pushBackward(7);
     linkedList.pushBackward(8);
     linkedList.pushBackward(9);
     linkedList.pushBackward(10);
 
+    // Print the linked list
     linkedList.printLinkedList();
     return 0;
 }
