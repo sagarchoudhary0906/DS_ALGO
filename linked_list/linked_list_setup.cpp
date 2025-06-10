9c51940fc9f3f5393f2e21c84db03d0e8f7d4d75
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-06-10 06:56:47 +0530

    pushing linked list basic setup

---
+++ linked_list/linked_list_setup.cpp
@@ -0,0 +1,96 @@
+/**
+ * A Linked LIst ALywas has only Two pointers Head and Tail
+ * Head is the first node of the list
+ * Tail is the last node of the list
+ * Head and Tail are pointers to the first and last node of the list respectively
+ * Head and Tail are used to traverse the list
+ * Head and Tail are used to insert and delete nodes from the list
+ * Head and Tail are used to search for a node in the list
+ * Head and Tail are used to sort the list
+ */
+
+/**
+ * Node is a class that represents a node in the linked list
+ * It has two attributes:
+ * 1. data: int
+ * 2. next: Node*
+ * 3. Constructor: Node(int data)
+ * 4. Getter and Setter for data and next
+ */
+
+#include <iostream>
+using namespace std;
+
+
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // Constructor
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class LinkedList
+{
+    // Attributes
+    Node *head;
+    Node *tail;
+
+public:
+    LinkedList()
+    {
+        this->head = NULL;
+        this->tail = NULL;
+    }
+
+    void pushForward(int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void printLinkedList()
+    {
+        Node *tempNode = head;
+        cout << "Printing LinkedList -> ";
+        while (tempNode != NULL)
+        {
+            int data = tempNode->data;
+            Node *nextNode = tempNode->next;
+            cout << data << "(" << nextNode << ")" << " -> ";
+            tempNode = nextNode;
+        }
+        cout << endl;
+        cout << "Printing Head Data now -> " << head->data;
+    }
+};
+
+int main()
+{
+    LinkedList linkedList = LinkedList();
+    linkedList.pushForward(5);
+    linkedList.pushForward(4);
+    linkedList.pushForward(3);
+    linkedList.pushForward(2);
+    linkedList.pushForward(1);
+    linkedList.printLinkedList();
+    return 0;
+}
